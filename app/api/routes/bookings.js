6349cf5fdf2460d9097bb6032efa1f49e9f68571
Olajumoke Oladimeji <jumoke5ng@yahoo.com>
2015-09-23 16:40:43 +0100

    Added the get hotel room and hotel booking endpoint TODO:complete the bookings

--- app/api/routes/bookings.js
+++ app/api/routes/bookings.js
@@ -2,6 +2,7 @@ var express = require('express'),
   mongoose = require('mongoose'),
   needle = require('needle'),
   Hotels = require('../../models/hotels'),
+  request = require('request'),
   _ = require('lodash');
 
 var jwt = require('jsonwebtoken');
@@ -9,11 +10,10 @@ var async = require('async');
 
 
 var saveToMongo = function(hotels) {
-  // Potato.collection.insert(potatoBag, onInsert);
   Hotels.remove({}, function(err) {
     if (err) {
       return;
-    };
+    }
     Hotels.collection.insert(hotels, function(err) {
       if (err) {
         res.send({
@@ -102,5 +102,47 @@ module.exports = function(app) {
     });
   });
 
+  // after selecting a hotel get all rooms in that hotel
+  app.route('/api/getHotelRooms/:hotelId').get(function(req, res) {
+      var hotelId = req.params.hotelId;
+      var url ='http://public.api.hotels.ng/api/api.php?cmd=get_hotel_rooms&hotel_id=' + hotelId;
+      needle.get(url, function(error, response) {
+        console.log('needle going ot get for you');
+      if (error) {
+        console.log('eerrr', error);
+        res.status(500).send({
+          message: error
+        });
+      }
+      if (!error && response.statusCode == 200) {
+        var responseArray = JSON.parse(response.body).data;
+         res.status(200).send({
+          count: responseArray.length,
+          data: responseArray
+         });
+      }
+      
+    });
+  });
+
+
+
+  app.route('/api/bookHotel').post(function(req, res) {
+
+    //should we hard code company id or will it come from the front end
+    var url = 'public.api.hotels.ng/api/api.php?cmd=make_booking';
+    var data = req.body;
+    needle.post(url, data, function(error, response) {
+      if (error) {
+        return res.status(500).send({
+          message: error
+        });
+      }
+      if (!error && response.statusCode == 200) {
+        console.log('response', response.body);
+        return;
+      }
+    });
+  });
+
 };
-// [{"id":"91719","name":"Prince Hotel","city":"Kano","statename":"Kano","total":"9777","images":[{"id":"261","image":"http://public.api.hotels.ng/slir/w611-h459-c4x3/views/91719/prince-hotel-kano-261.jpeg","thumbnail":"http://public.api.hotels.ng/slir/w80-h48-c81x49/views/91719/prince-hotel-kano-261.jpeg"},{"id":"257","image":"http://public.api.hotels.ng/slir/w611-h459-c4x3/views/91719/prince-hotel-kano-257.jpeg","thumbnail":"http://public.api.hotels.ng/slir/w80-h48-c81x49/views/91719/prince-hotel-kano-257.jpeg"},{"id":"258","image":"http://public.api.hotels.ng/slir/w611-h459-c4x3/views/91719/prince-hotel-kano-258.jpeg","thumbnail":"http://public.api.hotels.ng/slir/w80-h48-c81x49/views/91719/prince-hotel-kano-258.jpeg"},{"id":"259","image":"http://public.api.hotels.ng/slir/w611-h459-c4x3/views/91719/prince-hotel-kano-259.jpeg","thumbnail":"http://public.api.hotels.ng/slir/w80-h48-c81x49/views/91719/prince-hotel-kano-259.jpeg"},{"id":"260","image":"http://public.api.hotels.ng/slir/w611-h459-c4x3/views/91719/prince-hotel-kano-260.jpeg","thumbnail":"http://public.api.hotels.ng/slir/w80-h48-c81x49/views/91719/prince-hotel-kano-260.jpeg"},{"id":"262","image":"http://public.api.hotels.ng/slir/w611-h459-c4x3/views/91719/prince-hotel-kano-262.jpeg","thumbnail":"http://public.api.hotels.ng/slir/w80-h48-c81x49/views/91719/prince-hotel-kano-262.jpeg"},{"id":"263","image":"http://public.api.hotels.ng/slir/w611-h459-c4x3/views/91719/prince-hotel-kano-263.jpeg","thumbnail":"http://public.api.hotels.ng/slir/w80-h48-c81x49/views/91719/prince-hotel-kano-263.jpeg"}],"rating":[],"facilities":[{"id":"100","hotel_id":"91719","facility_id":"100","available":"0","comment":"","name":"Restaurant(s) ","type":"General","description":"","icon":"restaurant.png"},{"id":"101","hotel_id":"91719","facility_id":"101","available":"0","comment":"","name":"Bar/Lounge","type":"General","description":"","icon":"bar.png"},{"id":"103","hotel_id":"91719","facility_id":"103","available":"0","comment":"","name":"Security","type":"General","description":"","icon":"security.png"},{"id":"105","hotel_id":"91719","facility_id":"105","available":"0","comment":"","name":"Wireless Internet","type":"General","description":"","icon":"wireless.png"},{"id":"107","hotel_id":"91719","facility_id":"107","available":"0","comment":"","name":"Air Condition","type":"General","description":"","icon":"star.png"},{"id":"108","hotel_id":"91719","facility_id":"108","available":"0","comment":"","name":"24 Electricity","type":"General","description":"","icon":"electricity.png"},{"id":"179","hotel_id":"91719","facility_id":"179","available":"0","comment":"","name":"Adequate Parking Space","type":"Rooms","description":"","icon":"parking.png"},{"id":"180","hotel_id":"91719","facility_id":"180","available":"0","comment":"","name":"Swimming Pool","type":"Rooms","description":"","icon":"swimming.png"}]}
